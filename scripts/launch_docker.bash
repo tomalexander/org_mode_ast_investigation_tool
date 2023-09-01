4a556bc84f8b847c72f582b4c5043e20181abfba
Tom Alexander <tom@fizz.buzz>
2023-08-31 21:21:14 -0400

    Use read-only root for docker containers.

--- scripts/launch_docker.bash
+++ scripts/launch_docker.bash
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function main {
     make --directory "$DIR/../docker"
-    exec docker run --init --rm --publish 3000:3000/tcp org-investigation
+    exec docker run --init --rm --read-only --mount type=tmpfs,destination=/tmp --publish 3000:3000/tcp org-investigation
 }
 
 main "${@}"
